6156680b4f87c8af8cc65352530cedb66d712f2c
zgoda <zgoda@localhost>
2009-10-07 23:03:36 +0200

    Dodawanie wpis?w

--- bongo/views.py
+++ bongo/views.py
@@ -1,7 +1,27 @@
 # -*- coding: utf-8 -*-
 
-from bongo.utils import render_template
+from werkzeug import redirect
 
+from bongo.utils import expose, render_template
+from bongo.models import Entry
+from bongo.forms import EntryForm
+
+
+@expose('/')
 def index(request):
-    ctx = {}
+    form = EntryForm(request.form)
+    if request.method == 'POST' and form.validate():
+        entry = form.save()
+        return redirect(entry.get_url())
+    ctx = {
+        'form': form,
+        'entries': list(Entry.get_latest()),
+    }
     return render_template('index.html', **ctx)
+
+
+def entry(request, entry_id):
+    ctx = {
+        'entry': Entry.one({'_id': entry_id}),
+    }
+    return render_template('entry.html', **ctx)
