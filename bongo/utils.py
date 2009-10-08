4414043225e642f2ceb74174768da4c4a098c991
zgoda <zgoda@localhost>
2009-10-08 14:27:32 +0200

    Design jaki?, ?eby wygl?da?o

--- bongo/utils.py
+++ bongo/utils.py
@@ -7,12 +7,16 @@ from werkzeug import Response
 from werkzeug.routing import Map, Rule
 from jinja2 import Environment, FileSystemLoader
 
+from bongo import settings
+
 
 local = Local()
 local_manager = LocalManager([local])
 application = local('application')
 
-url_map = Map()
+url_map = Map(
+    [Rule('/media/<file>', endpoint='media', build_only=True)],
+)
 
 def expose(rule, **kw):
     def decorate(f):
@@ -27,5 +31,12 @@ def url_for(endpoint, _external=False, **values):
 env = Environment(loader=FileSystemLoader(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'templates')))
 env.globals['url_for'] = url_for
 
+def base_context():
+    return {
+        'SITE': settings.SITE,
+    }
+
 def render_template(template, **context):
-    return Response(env.get_template(template).render(**context), mimetype='text/html')
+    ctx = base_context()
+    ctx.update(context)
+    return Response(env.get_template(template).render(**ctx), mimetype='text/html')
