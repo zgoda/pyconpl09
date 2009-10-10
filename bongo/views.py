4f2e5b8184a9a09c11921ffdaab1d900e686f180
zgoda <zgoda@localhost>
2009-10-10 21:33:23 +0200

    404 gdy nie ma obiektu

--- bongo/views.py
+++ bongo/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from werkzeug import redirect
+from werkzeug.exceptions import NotFound
 
 from bongo.utils import expose, render_template
 from bongo.models import Entry
@@ -22,7 +23,10 @@ def index(request):
 
 @expose('/<entry_id>/')
 def entry(request, entry_id):
+    entry = Entry.one({'_id': entry_id})
+    if not entry:
+        raise NotFound
     ctx = {
-        'entry': Entry.one({'_id': entry_id}),
+        'entry': entry,
     }
     return render_template('entry.html', **ctx)
