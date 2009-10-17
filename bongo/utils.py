897c2a6a1608189cd86808620cd731150e0509cc
zgoda <jarek.zgoda@gmail.com>
2009-10-17 15:39:10 +0200

    Paginacja, paginacja, gdy jeste?my na wakacjach :D

--- bongo/utils.py
+++ bongo/utils.py
@@ -5,6 +5,7 @@ import os
 from werkzeug import Local, LocalManager
 from werkzeug import Response
 from werkzeug import import_string
+from werkzeug import cached_property
 from werkzeug.routing import Map, Rule
 from jinja2 import Environment, FileSystemLoader
 
@@ -43,3 +44,26 @@ def render_template(template, **context):
     ctx = base_context()
     ctx.update(context)
     return Response(env.get_template(template).render(**ctx), mimetype='text/html')
+
+
+class Pagination(object):
+
+    def __init__(self, objects, per_page, page, endpoint):
+        self.objects = objects
+        self.per_page = per_page
+        self.page = page
+        self.endpoint = endpoint
+
+    @cached_property
+    def count(self):
+        return self.objects.count()
+
+    @cached_property
+    def entries(self):
+        return self.objects.skip((self.page - 1) * self.per_page).limit(self.per_page)
+
+    has_previous = property(lambda x: x.page > 1)
+    has_next = property(lambda x: x.page < x.pages)
+    previous = property(lambda x: url_for(x.endpoint, page=x.page - 1))
+    next = property(lambda x: url_for(x.endpoint, page=x.page + 1))
+    pages = property(lambda x: max(0, x.count - 1) // x.per_page + 1)
