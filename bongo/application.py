b96bf36acd3e1d4306b6fcc95c933fb0d3ddff88
zgoda <jarek.zgoda@gmail.com>
2009-10-17 16:51:27 +0200

    Sesje

--- bongo/application.py
+++ bongo/application.py
@@ -5,6 +5,7 @@ import os
 from werkzeug import Request, ClosingIterator, SharedDataMiddleware
 from werkzeug import import_string
 from werkzeug.exceptions import HTTPException
+from werkzeug.contrib.sessions import SessionMiddleware
 
 from bongo.utils import local, local_manager, url_map
 from bongo import views
@@ -15,6 +16,10 @@ class Bongo(object):
 
     def __init__(self):
         local.application = self
+        storage_class = import_string(settings.SESSION_STORE)
+        if not os.path.isdir(settings.SESSION_PATH):
+            os.makedirs(settings.SESSION_PATH)
+        self.dispatch = SessionMiddleware(self.dispatch, storage_class(path=settings.SESSION_PATH))
         self.dispatch = SharedDataMiddleware(self.dispatch, {
             '/media': os.path.join(os.path.abspath(os.path.dirname(__file__)), 'media'),
         })
@@ -26,7 +31,7 @@ class Bongo(object):
         local.request = request = Request(environ)
         response = None
         for request_processor_name in settings.REQUEST_PROCESSORS:
-            processor = import_string(request_processor_name)
+            processor = import_string(request_processor_name)()
             response = processor.process_request(request)
             if response is not None:
                 break
@@ -37,7 +42,7 @@ class Bongo(object):
                 handler = getattr(views, endpoint)
                 response = handler(request, **values)
                 for request_processor_name in reversed(settings.REQUEST_PROCESSORS):
-                    processor = import_string(request_processor_name)
+                    processor = import_string(request_processor_name)()
                     response = processor.process_response(request, response)
             except HTTPException, e:
                 response = e
--- bongo/settings.py
+++ bongo/settings.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import os
+
+
+SESSION_STORE = 'werkzeug.contrib.sessions.FilesystemSessionStore'
+SESSION_PATH = os.path.join(os.environ['HOME'], 'var', 'sessiondata')
+
 SITE = {
     'name': u'Mongo-Bongo',
     'domain': u'localhost:5000',
@@ -10,4 +16,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'bongo.context_processors.latest',
 )
 
-REQUEST_PROCESSORS = ()
+REQUEST_PROCESSORS = (
+    'bongo.request_processors.SessionRequestProcessor',
+)
